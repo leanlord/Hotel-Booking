cf18bc8fd23ccebe6c8122cce0a699ad807f42fa
Vladimir <dadyka.vova@gmail.com>
2021-10-02 21:52:26 +0300

    create: added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button_next',
+    prevEl: '.slider-button_prev',
+  },
+});
